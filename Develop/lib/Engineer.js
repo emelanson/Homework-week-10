ea19bc6c124e26d78a779e0e84810059340c9368
emelanson <elijahmelanson@gmail.com>
2020-11-10 00:51:56 -0500

    Wrote Employee class. Began Engineer class.
    Added .gitignore for node_modules

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+
+    }
+
+    getId() {
+    }
+
+    getEmail() {
+    }
+
+    getRole() {
+        return "Employee"
+    }
+}

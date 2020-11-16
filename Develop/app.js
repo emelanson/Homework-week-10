c6bc63ee9737bdb1a4020a7e87597cb8b02d07c3
emelanson <elijahmelanson@gmail.com>
2020-11-16 15:40:38 -0500

    Created basic CLI prompt. Recursion of basic initial prompt question after each input.

--- Develop/app.js
+++ Develop/app.js
@@ -13,30 +13,41 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-let questions = [
+let initQuestion = [
     {
         type: "list",
         message: "What type of team member would you like to add?",
-        choices: ["Manager", "Engineer", "Intern"],
+        choices: ["Manager", "Engineer", "Intern", "END ADDITIONS"],
         name: "teamMember"
     },
 ]
 
-function init() {
+var initPrompt = (question) => {
     inquirer
-        .prompt(questions)
-        .then(employee => {
-            //Something happens
-        })
-        .catch(error => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-            } else {
-                // Something else when wrong
-            }
-        });
+        .prompt(question)
+        .then(function (ans) {
+            if (ans.teamMember === "Manager") {
+                console.log("Manager Branch");
+                return initPrompt(initQuestion);
 
+            } else if (ans.teamMember === "Engineer") {
+                console.log("Engineer");
+                return initPrompt(initQuestion);
+
+            } else if (ans.teamMember === "Intern") {
+                console.log("Intern");
+                return initPrompt(initQuestion);
+
+            } else if (ans.teamMember === "END ADDITIONS") {
+                console.log("Finishing up.  Thank you!");
+                return
+            }
+        })
+        .catch(err => { if (err) { console.log("There was an error!") } })
 }
+
+initPrompt(initQuestion);
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
